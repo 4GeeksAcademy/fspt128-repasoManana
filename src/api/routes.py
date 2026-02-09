6d84b3494e682e55939007fbbf170b148722780a
Angel Ponte <angeld0606@gmail.com>
2026-02-09 10:36:41 +0000

    login_backend_route ready

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -44,3 +45,28 @@ def signup():
     db.session.commit()
 
     return jsonify({"msg": "User created successfully"}), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email or not password:
+        return jsonify({"error": "Email and password are required"}), 400
+
+    user = db.session.execute(db.select(User).where(
+        User.email == email)).scalar_one_or_none()
+    
+    if user is None:
+        return jsonify({"error": "Invalid email or password"}), 401
+    
+    if user.check_password(password):
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({"msg": "Login successful", "token": access_token, "user": user.serialize()}), 200
+
+    else:
+        return jsonify({"error": "Invalid email or password"}), 401
+        
+
